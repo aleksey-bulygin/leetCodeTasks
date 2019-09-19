1a03eda1a606f25b1ccc7dc64ff6408d4270b690
Alex B <Strong.bulygin@bk.ru>
2019-09-19 14:44:54 +0300

    solution two tasks

---
+++ src/leetsCode/easy/PlusOneSolution.java
@@ -0,0 +1,45 @@
+package leetsCode.easy;
+
+/**
+ * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+ *
+ * The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+ *
+ * You may assume the integer does not contain any leading zero, except the number 0 itself.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3]
+ * Output: [1,2,4]
+ * Explanation: The array represents the integer 123.
+ *
+ * Example 2:
+ *
+ * Input: [4,3,2,1]
+ * Output: [4,3,2,2]
+ * Explanation: The array represents the integer 4321.
+ */
+public class PlusOneSolution {
+    public static void main(String[] args) {
+        System.out.println(new int[]{4,3,2,1});
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9)
+                digits[i] = 0;
+            else {
+                digits[i]++;
+                break;
+            }
+        }
+
+        if (digits[0] == 0) {
+            int[] newDigits = new int[digits.length + 1];
+            newDigits[0] = 1;
+            return newDigits;
+        }
+
+        return digits;
+    }
+}
